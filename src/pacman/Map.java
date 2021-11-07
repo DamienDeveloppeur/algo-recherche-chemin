956d4f558be95181b134c934178dec302a8b44c1
merrodev <damienlhaurado@hotmail.com>
2021-11-07 13:06:18 +0100

    last diffilcult, but bug

--- src/pacman/Map.java
+++ src/pacman/Map.java
@@ -36,30 +36,19 @@ public class Map {
     }
 
     public void generatePoint(){
-        for(int i =0; i < this.sizeMapHeight; i++) {
-            for (int j = 0; j < this.sizeMapWidth; j++) {
+        for(int i =0; i < this.sizeMapWidth; i++) {
+            for (int j = 0; j < this.sizeMapHeight; j++) {
                 boolean wall = true;
-                if(j == 3) {
-                    if (i == 1 || i == 2 || i == 3) {
-                        wall = false;
-                    }
-                }
-                if(j == 2 && i == 3) wall = false;
-                if(j == 1 && i == 3) wall = false;
-                if(j == 0 && i == 1) wall = false;
-
+                if (j == 5 && (i != 0 && i != 6)) wall = false;
+                if (i == 5 && (j != 0 && j != 6)) wall = false;
+                if (j == 1 && (i == 2 || i == 3 || i == 4 || i ==5))wall = false;
+                if(i == 0 && j == 2) wall = false;
                 p[i][j] = new Point(i, j, j+i, wall);
                 listNoeud.add(p[i][j]);
             }
         }
-        //startPoint = p[this.randInt()][this.randInt()];
         endPoint = p[4][3];
-         startPoint = p[0][0];
-        //System.out.println(endPoint);
-//        while(endPoint == null){
-//            if(p[this.randInt()][this.randInt()].isIfValide()) endPoint = p[this.randInt()][this.randInt()];
-//        }
-
+        startPoint = p[0][0];
         System.out.println(Map.endPoint.getCoord());
     }
 
@@ -69,39 +58,26 @@ public class Map {
     public void generateHeuristic(){
         for(int i =0; i < this.sizeMapHeight; i++) {
             for (int j = 0; j < this.sizeMapWidth; j++) {
-                //System.out.println(this.p[i][j].getPoint());
                 p[i][j].setHeuristic(Math.abs(p[i][j].getX() - Map.endPoint.getX()) + Math.abs(p[i][j].getY() - Map.endPoint.getY()));
                 System.out.println("coord : "+ p[i][j].getCoord() + " heuristique : " + p[i][j].getHeuristic());
             }
         }
     }
-
     // a ce stade, quel est le chemin le plus rapide entre le startPoint et le endPoint
     // chaque case checké sera enregistré dans un map
     public void findShortestPath() {
-        sizeRightPath = p[0][0].getHeuristic();
         currentPoint = startPoint;
-    System.out.println("______________________________________________________");
+        rightPath.add(startPoint);
+        System.out.println("______________________________________________________");
         while(!pathFind){
-//            for(Point p2 : listNoeud) {
-//                System.out.println(p2.getCoord());
-//            }
-
 
             boolean caseFinded = false;
-//            for(Point p3 : rightPath) {
-//                System.out.println(p3.getCoord());
-//            }
             caseChecked.clear();
-            // on check chaque case, on place ainsi chaque case checké dans une liste
-            // on a un point de tête, bloquing queue
-            // on compare l'heuristique de chaque
             for(Point p2 : listNoeud){
-                if(!rightPath.contains(p2)) {
+                if(!rightPath.contains(p2) && p2.isIfValide()) {
                     if(p2.getHeuristic() < currentPoint.getHeuristic() &&
                             p2.getX() - currentPoint.getX() <= 1 &&
                             p2.getY() - currentPoint.getY() <= 1 &&
-                            p2.isIfValide() &&
                             (p2.getX() - currentPoint.getX() == 0 ||
                              p2.getY() - currentPoint.getY() == 0)
                     ) {
@@ -117,27 +93,26 @@ public class Map {
                             for(Point p3 : rightPath) {System.out.println(p3.getCoord());}
                             return;
                         }
-
                     } else {
                         caseChecked.add(p2);
                     }
                 }
-
-
             }
             if(!caseFinded) {
-                System.out.println("case checkec 0 : " + caseChecked.get(0).getHeuristic());
-
-                float min =  caseChecked.get(0).getHeuristic();
-                Point pointToPush = caseChecked.get(0);
+                System.out.println("case checked 0 : " + caseChecked.get(0).getHeuristic());
+                float min = 9999999;
+                Point pointToPush = null;
                 for(int i = 0; i < caseChecked.size(); i++) {
-                    if(caseChecked.get(i).getHeuristic() < min &&
+//                    if (i == 0) {
+//                        min =  caseChecked.get(0).getHeuristic();
+//                        pointToPush = caseChecked.get(0);
+//                    }
+                    if((caseChecked.get(i).getHeuristic() < min || i == 0) &&
                         (caseChecked.get(i).getX() - currentPoint.getX() <= 1) &&
                         (caseChecked.get(i).getY() - currentPoint.getY() <= 1) &&
                         caseChecked.get(i).isIfValide() &&
                             (caseChecked.get(i).getX() - currentPoint.getX() == 0 ||
                             caseChecked.get(i).getY() - currentPoint.getY() == 0)
-
                     ) {
                         System.out.println("IN CASECHECKED p2.getX() - currentPoint.getX() : " + (caseChecked.get(i).getX() - currentPoint.getX()));
                         System.out.println("IN CASE CHECKED p2.getY() - currentPoint.getY() : "+(caseChecked.get(i).getY() - currentPoint.getY()));
@@ -152,6 +127,9 @@ public class Map {
                         pointToPush = caseChecked.get(i);
                     }
                 }
+                for(Point p3 : caseChecked) {
+                    System.out.println("case checked : "+ p3.getCoord());
+                }
                 if(!rightPath.contains(pointToPush)) {
                     rightPath.add(pointToPush);
                     currentPoint = pointToPush;
@@ -160,38 +138,7 @@ public class Map {
                 for(Point p3 : rightPath) {
                     System.out.println(p3.getCoord());
                 }
-
             }
         }
-        // code en python
-        /**
-         * frontier = PriorityQueue()
-         * frontier.put(start, 0)
-         * came_from = dict()
-         * cost_so_far = dict()
-         * came_from[start] = None
-         * cost_so_far[start] = 0
-         *
-         * while not frontier.empty():
-         *    current = frontier.get()
-         *
-         *    if current == goal:
-         *       break
-         *
-         *    for next in graph.neighbors(current):
-         *       new_cost = cost_so_far[current] + graph.cost(current, next)
-         *       if next not in cost_so_far or new_cost < cost_so_far[next]:
-         *          cost_so_far[next] = new_cost
-         *          priority = new_cost + heuristic(goal, next)
-         *          frontier.put(next, priority)
-         *          came_from[next] = current
-         */
     }
-    /**
-     *  XXX0X
-     *  XXXXX
-     *  XXXXX
-     *  X0XXX
-     *  XXXXX
-     */
 }
